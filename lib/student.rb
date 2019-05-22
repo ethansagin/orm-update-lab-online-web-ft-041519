49524d5120fba23566fc030380943686ce59efc4
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 21:00:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-    DB[:conn].execute(SELECT last_row_insertid() FROM )
+    DB[:conn].execute(SELECT last_row_insertid() FROM students)[0][0])
   end
 
 end
